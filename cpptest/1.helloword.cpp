b00f0df6592e977b890f86aaee46fd97362d93a1
Wang Zheng <1538310365@qq.com>
2024-09-21 23:24:28 +0800

    Create 1.helloword.cpp

---
+++ cpptest/1.helloword.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class name {
+public:
+	void print() {
+		printf("Hello,world!");
+	}
+private:
+};
+
+int main() {
+	name a;
+	a.print();
+	return 0;
+}
